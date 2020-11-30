a8eb8442c6081eb77d85813a585c040199837de4
Егор Соловьев <d-rom93@mail.ru>
2020-11-30 18:36:51 +0300

    Добавьте файлы проекта.

---
+++ Animation23/Animation23.cpp
@@ -0,0 +1,81 @@
+﻿#include<Windows.h>
+#include<tchar.h>
+#include "Resource.h"
+#include "animation.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+TCHAR WinName[] = _T("MainFrame");
+HINSTANCE hInst;
+int APIENTRY _tWinMain(HINSTANCE This,
+	HINSTANCE Prev,
+	LPTSTR cmd,
+	int mode) 
+{
+	HWND hWnd;
+	MSG msg;
+	hInst = This;
+	WNDCLASS wc;
+	wc.hInstance = This;
+	wc.lpszClassName = WinName;
+	wc.lpfnWndProc = WndProc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszMenuName = NULL; 
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(255, 255, 255));
+	if (!RegisterClass(&wc)) return 0;
+	hWnd = CreateWindow(WinName,
+		_T("Animation23"),
+		WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		HWND_DESKTOP,
+		NULL,
+		This,
+		NULL);
+	ShowWindow(hWnd, mode);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	static oa train;
+	static oa railways;
+	static int t = 0;
+	switch (message)
+	{
+	case WM_CREATE:
+		SetTimer(hWnd, 1, 10, NULL);
+		hdc = GetDC(hWnd);
+		train = oa(_T("train1.bmp"), hdc);
+		railways = oa(_T("pyti.bmp"), hdc);
+		ReleaseDC(hWnd, hdc);
+		break;
+	case WM_TIMER:
+		t++;
+		if (t > railways.bm.bmWidth) t = -1 * train.bm.bmWidth;
+		InvalidateRect(hWnd, NULL, TRUE);
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		railways.Draw(0, 500, hdc);
+		train.Draw(t, -2, hdc);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:PostQuitMessage(0); break;
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
